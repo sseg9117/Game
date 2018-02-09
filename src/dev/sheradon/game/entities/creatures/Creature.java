b0c0a138a8eb1a80f528bc4beb9cb485adc40c1a
Sheradon <sherbear242@github.com>
2018-02-09 11:18:55 -0800

    added classes for states.

---
+++ src/dev/sheradon/game/entities/creatures/Creature.java
@@ -0,0 +1,13 @@
+package dev.sheradon.game.entities.creatures;
+
+import dev.sheradon.game.entities.Entity;
+
+public abstract class Creature extends Entity
+{
+
+	public Creature(float x, float y)
+	{
+		super(x, y);
+	}
+	
+}
