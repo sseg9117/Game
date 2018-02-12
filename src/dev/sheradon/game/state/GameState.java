1fa3a24ed5840cf63071491f36314b624e590fb6
Sheradon <sherbear242@github.com>
2018-02-12 11:28:44 -0800

    added basic worlds.

--- src/dev/sheradon/game/state/GameState.java
+++ src/dev/sheradon/game/state/GameState.java
@@ -5,26 +5,31 @@ import java.awt.Graphics;
 import dev.sheradon.game.Game;
 import dev.sheradon.game.entities.creatures.Player;
 import dev.sheradon.game.tile.Tile;
+import dev.sheradon.game.worlds.World;
 
 public class GameState extends State
 {
 	private Player player;
+	private World world;
 	
 	public GameState(Game game)
 	{
 		super(game);
 		player = new Player(game, 100, 100);
+		world = new World("res/worlds/world1.txt");
 	}
 
 	@Override
 	public void tick()
 	{
+		world.tick();
 		player.tick();
 	}
 
 	@Override
 	public void render(Graphics g)
 	{
+		world.render(g);
 		player.render(g);
 		Tile.tiles[2].render(g, 0, 0);
 	}
