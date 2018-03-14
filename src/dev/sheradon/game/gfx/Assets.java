2d606daae8b639523977b5e768d762c342398fee
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-03-14 08:33:29 -0600

    re organized.

--- src/dev/sheradon/game/gfx/Assets.java
+++ src/dev/sheradon/game/gfx/Assets.java
@@ -17,22 +17,26 @@ public class Assets
 				ImageLoader.loadImage("/textures/sheet.png"));
 		
 		
-		player_right = new BufferedImage[8];
-		player_left = new BufferedImage[8];
-		player_up = new BufferedImage[1];
-		player_down = new BufferedImage[1];
-		player_still = new BufferedImage[1];
+		
+		grass = sheet.crop(width * 0, 0, width, height);
+		stone = sheet.crop(width * 1, 0, width, height);
+		dirt = sheet.crop(width * 2, 0, width, height);
+		tree = sheet.crop(width * 0, height * 1, width, height * 2);
 		
 		btn_start = new BufferedImage[2];
 		btn_start[0] = sheet.crop(width *6, height *4, width *2, height);
 		btn_start[1] = sheet.crop(width *6, height *5, width *2, height);
 		
+		player_still = new BufferedImage[1];
 		player_still[0] = sheet.crop(width * 4, height * 3, width, height);
 		
+		player_up = new BufferedImage[1];
 		player_up[0] = sheet.crop(width * 4, height * 3, width, height);
 
+		player_down = new BufferedImage[1];
 		player_down[0] = sheet.crop(width * 4, height * 3, width, height);
 
+		player_right = new BufferedImage[8];
 		player_right[0] = sheet.crop(width * 3, 0, width, height);
 		player_right[1] = sheet.crop(width * 4, 0, width, height);
 		player_right[2] = sheet.crop(width * 5, 0, width, height);
@@ -42,6 +46,7 @@ public class Assets
 		player_right[6] = sheet.crop(width * 4, height * 1, width, height);
 		player_right[7] = sheet.crop(width * 5, height * 1, width, height);
 		
+		player_left = new BufferedImage[8];
 		player_left[7] = sheet.crop(width * 6, height * 1, width, height);
 		player_left[6] = sheet.crop(width * 7, height * 1, width, height);
 		player_left[5] = sheet.crop(width * 3, height * 2, width, height);
@@ -50,11 +55,6 @@ public class Assets
 		player_left[2] = sheet.crop(width * 6, height * 2, width, height);
 		player_left[1] = sheet.crop(width * 7, height * 2, width, height);
 		player_left[0] = sheet.crop(width * 3, height * 3, width, height);
-
-		grass = sheet.crop(width * 0, 0, width, height);
-		stone = sheet.crop(width * 1, 0, width, height);
-		dirt = sheet.crop(width * 2, 0, width, height);
-		tree = sheet.crop(width * 0, height * 1, width, height * 2);
 
 	}
 }
