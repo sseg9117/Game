daefa89074d6a1883182086b8ce54e3826227dfd
Sheradon <sherbear242@github.com>
2018-04-10 07:00:52 -0600

    tried somthing

---
+++ src/dev/sheradon/game/entities/statics/Chest.java
@@ -0,0 +1,43 @@
+package dev.sheradon.game.entities.statics;
+
+import java.awt.Graphics;
+
+import dev.sheradon.game.Handler;
+import dev.sheradon.game.gfx.Assets;
+import dev.sheradon.game.items.Item;
+import dev.sheradon.game.tile.Tile;
+//Might have used something fron stackoverflow but dont remember
+public class Chest extends StaticEntity
+{
+
+	public Chest(Handler handler, float x, float y)
+	{
+		super(handler, x, y, Tile.TILEWIDTH, Tile.TILEHEIGHT * 2);
+
+		bounds.x = (int) (20.55f);
+		bounds.y = (int) (height / 2.3f);
+		bounds.width = width - 40;
+		bounds.height = (int) (height - height / 2.1f);
+	}
+
+	@Override
+	public void tick()
+	{
+
+	}
+	@Override
+	public void die()
+	{
+		handler.getWorld().getItemManager().addItem(Item.woodItem.createNew((int) x,(int)  y));
+	}
+	@Override
+	public void render(Graphics g)
+	{
+		g.drawImage(Assets.chest,
+				(int) (x - handler.getGameCamera().getXoffset()),
+				(int) (y - handler.getGameCamera().getYoffset()), width, height,
+				null);
+
+	}
+
+}
--- src/dev/sheradon/game/gfx/Assets.java
+++ src/dev/sheradon/game/gfx/Assets.java
@@ -6,7 +6,7 @@ public class Assets
 {
 	private static final int width = 32, height = 32;
 
-	public static BufferedImage dirt, sky, grass, stone, tree, wood, lava, leaves;
+	public static BufferedImage dirt, sky, grass, stone, tree, wood, lava, leaves, chest;
 	public static BufferedImage[] player_right, player_left, player_up,
 			player_down, player_still, player_jump, player2_right, player2_left, player2_up,
 			player2_down, player2_still, player2_jump;
@@ -22,11 +22,14 @@ public class Assets
 		grass = sheet.crop(width * 0, 0, width, height);
 		stone = sheet.crop(width * 1, 0, width, height);
 		dirt = sheet.crop(width * 2, 0, width, height);
-		tree = sheet.crop(width * 0, height * 1, width, height * 2);
 		sky = sheet.crop(width * 2, height * 1, width, height);
 		leaves = sheet.crop(width * 1, height * 2, width, height);
 		wood = sheet.crop(width * 1, height * 1, width, height);
 		lava = sheet.crop(width * 2, height * 2, width, height);
+		
+		tree = sheet.crop(width * 0, height * 1, width, height * 2);
+		chest = sheet.crop(width * 0, height * 5, width, height);
+		
 		
 		player2_jump = new BufferedImage[1];
 		player2_jump[0] = sheet.crop(width * 3, height *4, width, height);
